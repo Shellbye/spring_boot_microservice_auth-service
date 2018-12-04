bdd320dade8ba504a4b3086abd8b4a8df8beddff
shellbye <shellbye.com@gmail.com>
2018-12-04 20:16:23 +0800

    EnableEurekaClient

--- src/main/java/com/shellbye/auth/AuthApplication.java
+++ src/main/java/com/shellbye/auth/AuthApplication.java
@@ -2,8 +2,10 @@ package com.shellbye.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class AuthApplication {
 
     public static void main(String[] args) {
